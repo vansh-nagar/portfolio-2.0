87a7dcfed31ce55ef27c2b8e89b86764266924f4
vansh-nagar <189650812+vansh-nagar@users.noreply.github.com>
2025-11-06 22:24:29 +0530

    fix: update Orcha project image URL to the latest version

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -31,7 +31,7 @@ export const ProjectsArr = [
     name: "Orcha",
     link: "http://orcha.site/",
     image:
-      "https://res.cloudinary.com/dz12pywzs/image/upload/v1762146870/Copy_of_Webinar_Keynote_Presentation_1_ucqgh3.jpg",
+      "https://res.cloudinary.com/dz12pywzs/image/upload/v1762448012/Copy_of_Webinar_Keynote_Presentation_1_rtllsv.jpg",
     github: "https://github.com/vansh-nagar/orcha",
     demo: "http://orcha.site/",
     figmaFile: "",
